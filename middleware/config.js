69e1d8929adfe0e152a1b381cd0ad2c518ec1332
Your Name <your_email@example.com>
2026-03-06 21:28:16 +0500

    Add

---
+++ middleware/config.js
@@ -0,0 +1,14 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: process.env.ADMIN_PASS,
+  },
+  debug: true,
+});
+
+module.exports = transporter;
